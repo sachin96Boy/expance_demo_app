00677072736c94b2e3976209ae839a42bcf9df6b
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-03-15 17:10:56 +0530

    chor: device orientat adjust

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../pages/home_page.dart';
 
 void main() {
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
